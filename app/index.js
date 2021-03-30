e1d86437485eb88914265bac072de01871f6df11
tedski999 <tedjohnsonjs@gmail.com>
2021-03-30 22:25:44 +0100

    Restarting work on project
    Removed most application work
    Changed license to GPLv3
    Updated ESLint configuration
    Cleaned up package.json
    Split up ./data/config.json

--- app/index.js
+++ app/index.js
@@ -1,28 +1,9 @@
-"use strict";
-const process = require("process");
-const Discord = require("discord.js");
-const clientHandler = require("./clientHandler.js");
-const guildHandler = require("./guildHandler.js");
-const messageHandler = require("./messageHandler.js");
-const utils = require("./utils.js");
-const privateData = require("../data/private.json");
+const { Client } = require("discord.js");
+const { token } = require("../data/private.json");
+const client = new Client();
 
-utils.log("Setting up bot...");
-let client = new Discord.Client();
+client.on("ready", () => {
+  console.log("smh my head here we go again");
+});
 
-utils.log("Setting up event callbacks...", utils.LOG_LEVEL.VERBOSE);
-client.on("debug", (info) => process.env.DEBUG ? utils.log("[DISCORDJS]: " + info, utils.LOG_LEVEL.VERBOSE) : null);
-client.on("warn", (info) => utils.log("[DISCORDJS]: " + info, utils.LOG_LEVEL.WARNING));
-client.on("error", (error) => utils.log("[DISCORDJS]: " + error, utils.LOG_LEVEL.ERROR));
-client.on("ready", () => clientHandler.onReady(client));
-client.on("shardReady", (id, unavailableGuilds) => clientHandler.onShardReady(id, unavailableGuilds));
-client.on("shardResume", (id, replayedEvents) => clientHandler.onShardResume(id, replayedEvents));
-client.on("shardError", (error, id) => clientHandler.onShardError(error, id));
-client.on("shardReconnecting", (id) => clientHandler.onShardReconnecting(id));
-client.on("shardDisconnect", (event, id) => clientHandler.onShardDisconnect(event, id));
-client.on("message", (message) => messageHandler.onMessage(message));
-client.on("guildCreate", (guild) => guildHandler.onGuildCreate(guild));
-client.on("guildDelete", (guild) => guildHandler.onGuildDelete(guild));
-
-utils.log("Logging into Discord...", utils.LOG_LEVEL.VERBOSE);
-client.login(privateData.key);
+client.login(token);
--- app/messageHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const utils = require("./utils.js");
-
-function onMessage(message) {
-	utils.log(utils.formatText("Message: %s", message), utils.LOG_LEVEL.ERROR);
-}
-
-module.exports = { onMessage };
